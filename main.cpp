2eca7114d4a9608cf0f3faaeb5059d9ada56c6c3
oltijonkadolli <oltijon.kadolli@student.uni-pr.edu>
2026-01-03 17:37:05 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "student.h"
+#include "statistics.h"
+
+int main()
+{
+    shtoNxenes(5);
+    shtoNxenes(96);
+    shfaqTotalin();
+    largoNxenes(3);
+    shfaqStatistikat();
+
+    return 0;
+}
